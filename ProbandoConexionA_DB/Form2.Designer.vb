11b9407b91576a42e5f10fcc5359e36dfcecc1f0
Javier D'Alessandro <javierosvaldodalessandro@gmail.com>
2023-10-06 17:39:59 -0300

    Actualizado

--- ProbandoConexionA_DB/Form2.Designer.vb
+++ ProbandoConexionA_DB/Form2.Designer.vb
@@ -28,34 +28,52 @@ Partial Class Form2
         Me.Nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Edad = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Antiguedad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Estado = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Estado = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Nombre_sis = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Apellido_sis = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Edad_sis = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.sexo_sis = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(364, 23)
+        Me.Button1.Location = New System.Drawing.Point(323, 7)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Ver Datos"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'DataGridView1
         '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Apellido, Me.Nombre, Me.Edad, Me.Antiguedad, Me.Estado})
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 145)
+        Me.DataGridView1.Location = New System.Drawing.Point(70, 76)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(742, 150)
+        Me.DataGridView1.Size = New System.Drawing.Size(676, 87)
         Me.DataGridView1.TabIndex = 1
         '
         'Apellido
         '
         Me.Apellido.HeaderText = "Apellido"
         Me.Apellido.Name = "Apellido"
+        Me.Apellido.Width = 200
         '
         'Nombre
         '
@@ -72,9 +90,14 @@ Partial Class Form2
         Me.Antiguedad.HeaderText = "Antiguedad"
         Me.Antiguedad.Name = "Antiguedad"
         '
+        'Estado
+        '
+        Me.Estado.HeaderText = "Estado"
+        Me.Estado.Name = "Estado"
+        '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(128, 42)
+        Me.TextBox1.Location = New System.Drawing.Point(161, 9)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 2
@@ -82,22 +105,141 @@ Partial Class Form2
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(125, 23)
+        Me.Label1.Location = New System.Drawing.Point(28, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(107, 13)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "INGRESAR LEGAJO"
         '
-        'Estado
+        'Label2
         '
-        Me.Estado.HeaderText = "Estado"
-        Me.Estado.Name = "Estado"
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(76, 268)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nombre"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(136, 268)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(136, 305)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(76, 305)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Apellido"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(136, 342)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(136, 379)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(76, 342)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Edad"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(76, 379)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Sexo"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(202, 211)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(247, 18)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Carga tabla Empleados sistema"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(148, 415)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Grabar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Nombre_sis, Me.Apellido_sis, Me.Edad_sis, Me.sexo_sis})
+        Me.DataGridView2.Location = New System.Drawing.Point(393, 268)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(542, 150)
+        Me.DataGridView2.TabIndex = 15
+        '
+        'Nombre_sis
+        '
+        Me.Nombre_sis.HeaderText = "Nombre"
+        Me.Nombre_sis.Name = "Nombre_sis"
+        '
+        'Apellido_sis
+        '
+        Me.Apellido_sis.HeaderText = "  Apellido"
+        Me.Apellido_sis.Name = "Apellido_sis"
+        '
+        'Edad_sis
+        '
+        Me.Edad_sis.HeaderText = "Edad"
+        Me.Edad_sis.Name = "Edad_sis"
+        '
+        'sexo_sis
+        '
+        Me.sexo_sis.HeaderText = "Sexo"
+        Me.sexo_sis.Name = "sexo_sis"
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(947, 490)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.DataGridView1)
@@ -105,6 +247,7 @@ Partial Class Form2
         Me.Name = "Form2"
         Me.Text = "Form2"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -112,11 +255,26 @@ Partial Class Form2
 
     Friend WithEvents Button1 As Button
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button2 As Button
     Friend WithEvents Apellido As DataGridViewTextBoxColumn
     Friend WithEvents Nombre As DataGridViewTextBoxColumn
     Friend WithEvents Edad As DataGridViewTextBoxColumn
     Friend WithEvents Antiguedad As DataGridViewTextBoxColumn
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Label1 As Label
     Friend WithEvents Estado As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Nombre_sis As DataGridViewTextBoxColumn
+    Friend WithEvents Apellido_sis As DataGridViewTextBoxColumn
+    Friend WithEvents Edad_sis As DataGridViewTextBoxColumn
+    Friend WithEvents sexo_sis As DataGridViewTextBoxColumn
 End Class
--- ProbandoConexionA_DB/Form2.vb
+++ ProbandoConexionA_DB/Form2.vb
@@ -2,17 +2,24 @@
 
 Public Class Form2
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim leg As Integer
+        Dim leg As String
         leg = TextBox1.Text
         Dim resultado As Boolean
         Dim transa As SqlTransaction = Nothing
         Dim mensaje As String = Nothing
         Dim resul1 As DataTable = Nothing
-
+        If leg = "" Then
+            MessageBox.Show("No ha ingresado un nuemro de legajo")
+            Exit Sub
+        End If
         'Dim libsql As New ClsLibreriaSql
+        'Dim leg1 As Integer = 22
+
         libSql.AbrirConexion(resultado, mensaje)
+
         If resultado Then
-            libSql.Consulta("dbo.AAAA_BORRAR " & leg, resul1, transa, resultado, mensaje)
+            SQL = "dbo.AAAA_BORRAR " + leg
+            libSql.Consulta(SQL, resul1, transa, resultado, mensaje)
             If resultado Then
                 If resul1.Rows.Count > 0 Then
 
@@ -37,4 +44,67 @@ Public Class Form2
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
     End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim Ape As String
+        Dim Nom As String
+        Dim Edad As Integer
+        Dim Sex As String
+        Dim resultado As Boolean
+        Dim transa As SqlTransaction = Nothing
+        Dim mensaje As String = Nothing
+        Dim resul2 As DataTable = Nothing
+        Dim devuelve As Integer
+
+        Ape = TextBox2.Text
+        Nom = TextBox3.Text
+        Edad = TextBox4.Text
+        Sex = TextBox5.Text
+
+        'MessageBox.Show(Ape & ", " & ", " & Nom & ", " & Edad & ", " & " ," & Sex)
+        libSql.AbrirConexion(resultado, mensaje)
+
+        If resultado Then
+
+
+            SQL = "INSERT INTO [Sistema].[dbo].[AAAA_Emplados_Sisitema]
+           ([Apellido_sis]
+           ,[Nombre_sis]
+           ,[Edad_sis]
+           ,[Sexo_sis])
+            VALUES
+           ('" & Ape & "', '" & Nom & "', '" & Edad & "', '" & Sex & "')"
+            libSql.Ejecutar(SQL, True,devuelve,transa,resultado,mensaje)
+
+            'libSql.Consulta(SQL, resul2, transa, resultado, mensaje)
+        Else
+            MessageBox.Show("NO se conecto")
+
+        End If
+
+        libSql.AbrirConexion(resultado, mensaje)
+
+        SQL = "SELECT * FROM AAAA_Emplados_Sisitema WHERE id = '" & devuelve & "';"
+
+        libSql.Consulta(SQL, resul2, transa, resultado, mensaje)
+
+        'DataGridView2.Rows(0).Cells("id").Value = resul2.Rows(0).Item("id")
+        DataGridView2.Rows(0).Cells("Apellido_sis").Value = resul2.Rows(0).Item("Apellido_sis")
+        DataGridView2.Rows(0).Cells("Nombre_sis").Value = resul2.Rows(0).Item("Nombre_sis")
+        DataGridView2.Rows(0).Cells("Edad_sis").Value = resul2.Rows(0).Item("Edad_sis")
+        DataGridView2.Rows(0).Cells("Sexo_sis").Value = resul2.Rows(0).Item("Sexo_sis")
+
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+
 End Class
